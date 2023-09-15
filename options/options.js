a21bfb049e7dc323de14d79929e96a8d19e7daa2
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 09:49:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-feather-dev-116802",
+  url: "https://broad-feather-dev-116802.botics.co",
+  api: "https://broad-feather-dev-116802.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
